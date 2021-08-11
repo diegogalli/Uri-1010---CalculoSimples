f00f4c74e8152427ecc11b8eff3030e162b250d9
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:07:32 -0300

    Uri 1010 Calculo Simples

---
+++ CalculoSimples/CalculoSimples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CodPc1, CodPc2, NrPc1, NrPc2;
+            double ValorPc1, ValorPc2, ValorPago;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            CodPc1 = int.Parse(valores[0]);
+            NrPc1 = int.Parse(valores[1]);
+            ValorPc1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            CodPc2 = int.Parse(valores[0]);
+            NrPc2 = int.Parse(valores[1]);
+            ValorPc2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            ValorPago = ValorPc1 * NrPc1 + ValorPc2 * NrPc2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + ValorPago.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
